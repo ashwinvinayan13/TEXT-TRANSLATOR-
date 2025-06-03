a679656dda2f80540f2a7c78be7f8660649da441
ashwinvinayan13 <ashwinvinayan1306@gmail.com>
2025-06-03 10:36:16 +0530

    Updated UI

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
-translator = pipeline("translation to english to malayalam ", model="Helsinki-NLP/opus-mt-en-ml")
+translator = pipeline("translation_en_to_ml", model="Helsinki-NLP/opus-mt-en-ml")
 
 app = FastAPI()
 
 class TextInput(BaseModel):
     text: str
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def home():
+    return FileResponse("static/index.html")
+
+@app.post("/translate")
+async def translate_text(data: TextInput):
+    result = translator(data.text)
+    return {"translation": result[0]["translation_text"]}
+
